c93eeb5224d505bf5b1e51b49fe20ce2f6c22114
Sairaj Mestry <mestry.sairaj1506@gmail.com>
2025-11-06 19:05:30 +0530

    Update hittable.cpp

--- with_bvh/hittable.cpp
+++ with_bvh/hittable.cpp
@@ -79,3 +79,82 @@ bool RectPlane::bounding_box(double, double, AABB &box) const {
     box = AABB(minp, maxp);
     return true;
 }
+
+BVHNode::BVHNode(std::vector<std::shared_ptr<Hittable>> &objects, size_t start, size_t end) {
+    std::random_device rd;
+    std::default_random_engine rng(rd());
+    int axis = int(3 * rand01());
+    auto comparator = (axis == 0) ? box_x_compare
+                    : (axis == 1) ? box_y_compare
+                                  : box_z_compare;
+
+    size_t object_span = end - start;
+
+    if (object_span == 1) {
+        left = right = objects[start];
+    } else if (object_span == 2) {
+        if (comparator(objects[start], objects[start+1])) {
+            left = objects[start];
+            right = objects[start+1];
+        } else {
+            left = objects[start+1];
+            right = objects[start];
+        }
+    } else {
+        std::sort(objects.begin() + start, objects.begin() + end, comparator);
+        size_t mid = start + object_span / 2;
+        left = std::make_shared<BVHNode>(objects, start, mid);
+        right = std::make_shared<BVHNode>(objects, mid, end);
+    }
+
+    AABB box_left, box_right;
+    if (!left->bounding_box(0,0, box_left) || !right->bounding_box(0,0, box_right))
+        std::cerr << "No bounding box in BVHNode constructor.\n";
+    box = surrounding_box(box_left, box_right);
+}
+
+bool BVHNode::hit(const Ray &r, double tmin, double tmax, HitRecord &rec) const {
+    if (!box.hit(r, tmin, tmax)) return false;
+    HitRecord leftRec, rightRec;
+    
+    bool hit_left = left->hit(r, tmin, tmax, leftRec);
+    bool hit_right = right->hit(r, tmin, hit_left ? leftRec.t : tmax, rightRec);
+
+    if (hit_right) {
+        rec = rightRec;
+        return true;
+    }
+
+    if (hit_left) {
+        rec = leftRec;
+        return true;
+    }
+
+    return false;
+}
+
+bool BVHNode::bounding_box(double, double, AABB &out_box) const {
+    out_box = box;
+    return true;
+}
+
+bool BVHNode::box_compare(const std::shared_ptr<Hittable> a, const std::shared_ptr<Hittable> b, int axis) {
+    AABB box_a, box_b;
+    if (!a->bounding_box(0,0,box_a) || !b->bounding_box(0,0,box_b))
+        std::cerr << "No bounding box in box_compare.\n";
+    if (axis == 0) return box_a.min().x < box_b.min().x;
+    else if (axis == 1) return box_a.min().y < box_b.min().y;
+    else return box_a.min().z < box_b.min().z;
+}
+
+bool BVHNode::box_x_compare(const std::shared_ptr<Hittable> a, const std::shared_ptr<Hittable> b) { 
+    return box_compare(a,b,0); 
+}
+
+bool BVHNode::box_y_compare(const std::shared_ptr<Hittable> a, const std::shared_ptr<Hittable> b) { 
+    return box_compare(a,b,1); 
+}
+
+bool BVHNode::box_z_compare(const std::shared_ptr<Hittable> a, const std::shared_ptr<Hittable> b) { 
+    return box_compare(a,b,2); 
+}
