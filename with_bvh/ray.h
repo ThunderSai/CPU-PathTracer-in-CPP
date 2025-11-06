3351f804728bee85fd4bffa3d6cbc95870e16874
Shubham7shubh <b24ee1074@iitj.ac.in>
2025-11-06 18:53:13 +0530

    Create ray.h

---
+++ with_bvh/ray.h
@@ -0,0 +1,19 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+struct Ray { 
+    Vec3 o,d; 
+    
+    Ray(){} 
+
+    Ray(const Vec3 &o_, const Vec3 &d_): o(o_), d(d_){}
+
+    Vec3 at(double t) const { 
+        return o + t*d;
+    } 
+
+};
+
+#endif
