e587f3cd01c72c64b83fde2fd995d1565c99eefa
Shubham7shubh <b24ee1074@iitj.ac.in>
2025-11-14 00:41:44 +0530

    Update material.h

--- with_bvh/include/material.h
+++ with_bvh/include/material.h
@@ -6,32 +6,37 @@
 struct Ray;
 struct HitRecord;
 
+// Base class sab materials ke liye
+// Ye batata hai ray surface pe aake kya karegi
 struct Material {
+// scatter: ray ka naya direction aur color decide karta hai scatter in random directio
     virtual bool scatter(const Ray &in, const HitRecord &rec, Vec3 &attenuate, Ray &scattered) const = 0;
+ // emitted: agar material light nikalta hai to yaha se color milega
+
     virtual Vec3 emitted() const { 
         return Vec3(0,0,0); 
     }
     virtual ~Material() = default;
 };
-
+// Lambert: matte surface (painted wall jaisa)
 struct Lambert: public Material {
     Vec3 albedo;
     Lambert(const Vec3 &a): albedo(a) {}
     bool scatter(const Ray&, const HitRecord &rec, Vec3 &attenuate, Ray &scattered) const override;
 };
-
+// Metal: shiny surface (mirror jaisa)
 struct Metal: public Material {
-    Vec3 albedo; double fuzz;
+    Vec3 albedo; double fuzz; // fuss to add randomness for rough metal
     Metal(const Vec3 &a, double f): albedo(a), fuzz(f < 1.0 ? f : 1.0) {}
     bool scatter(const Ray &in, const HitRecord &rec, Vec3 &attenuate, Ray &scattered) const override;
 };
-
+// Dielectric: transparent (glass/water) we use snails law and schlet law
 struct Dielectric: public Material {
     double ir;
     Dielectric(double index): ir(index) {}
     bool scatter(const Ray &in, const HitRecord &rec, Vec3 &attenuate, Ray &scattered) const override;
 };
-
+// DiffuseLight is used if material itself is the light source.
 struct DiffuseLight: public Material {
     Vec3 emit;
     DiffuseLight(const Vec3 &c): emit(c) {}
