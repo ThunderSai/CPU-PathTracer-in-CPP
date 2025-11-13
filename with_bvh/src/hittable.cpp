f1af4a85755f12a6e59b350fa8bbcb3e256ae4d0
madhurvishwakarma <b24ee1041@iitj.ac.in>
2025-11-13 19:36:07 +0530

    Update hittable.cpp

--- with_bvh/src/hittable.cpp
+++ with_bvh/src/hittable.cpp
@@ -4,60 +4,75 @@
 #include <random>
 #include <iostream>
 #include <cmath>
-
+// function to check whether the ray(r) hits the sphere in the given range ( tmin to tmax ) or not 
 bool Sphere::hit(const Ray &r, double tmin, double tmax, HitRecord &rec) const {
-    Vec3 oc = r.o - center;
+    Vec3 oc = r.o - center; // line to compute the vector oc from the center( center of the sphere ) to the origin of the ray ( r. o ) 
+    // constants a , b , c came after simplifying the quadratic eqaution that came from solving the inetrsection of sphere and the ray 
+    // basically we can say that these are the quadratic coefficients for intersection of sphere and the ray 
     double a = dot(r.d, r.d);
-    double half_b = dot(oc, r.d);
+    double half_b = dot(oc, r.d); // instead of b the term b_half is taken for better and simple calculation
     double c = dot(oc,oc) - radius*radius;
+    // code to calculate and the discriminent and check whether the ray hits the sphere or not 
     double disc = half_b*half_b - a*c;
-    if (disc <= 0) return false;
+    if (disc <= 0) return false; // if disc < 0 the ray did not hit the sphere and if it id (>0) then it means ray hits the sphere and there exists atleast one solution 
+    // code to calculate roots 
     double sq = sqrt(disc);
     double root = (-half_b - sq)/a;
+    // we would prefer the smaller root as the smaller would be the root = closure to the sphere 
+    // but if that root is not in the given range then we would check for the large root or farther root 
     if (root < tmin || root > tmax) {
         root = (-half_b + sq)/a;
         if (root < tmin || root > tmax) {
             return false;
         }
     }
-    rec.t = root;
-    rec.p = r.at(rec.t);
-    Vec3 outward = (rec.p - center) / radius;
-    rec.set_face_normal(r, outward);
-    rec.mat = mat;
+    rec.t = root; // intersection distance measured along the ray 
+    rec.p = r.at(rec.t); // point of intersection 
+    Vec3 outward = (rec.p - center) / radius; // calculation for the normal to the surface 
+    rec.set_face_normal(r, outward); // to ensure that the normal is anti - parallel to the ray or in the opposite direction of the ray 
+    rec.mat = mat; // stores pointer for shading purpose of the sphere 
     return true;
 }
-
+// creates a boundary ( AABB ) around the sphere where the min. point would be first term in AABB and the max. would be the second term in AABB   
 bool Sphere::bounding_box(double, double, AABB &box) const {
     box = AABB(center - Vec3(radius, radius, radius), center + Vec3(radius, radius, radius));
     return true;
 }
-
+// represents a rectangular plane ( finite ) over an infinite plane 
+// initializer for the rectangular plane geomatry where the terms used in it are discussed below 
+// origin ( refrence point that can be center or any corner ) , u_dir and v_dir are the direction vector along the two sides , u and v ( max , min ) are the limits for the rectangle  
 RectPlane::RectPlane(const Vec3 &o, const Vec3 &n, const Vec3 &ud, const Vec3 &vd, 
                      double umin_, double umax_, double vmin_, double vmax_, std::shared_ptr<Material> m)
     : origin(o), normal(n.normalized()), u_dir(ud.normalized()), v_dir(vd.normalized()), 
       umin(umin_), umax(umax_), vmin(vmin_), vmax(vmax_), mat(m) {}
-
+// function that check if the ray hits the rectangular plane or not 
 bool RectPlane::hit(const Ray &r, double tmin, double tmax, HitRecord &rec) const {
+    // code to find the point of intersection 
     double denom = dot(r.d, normal);
     if (fabs(denom) < 1e-8) {
         return false;
     }
     double t = dot(origin - r.o, normal) / denom;
+    // check for intersection point that the intersection point lies in the given range or not 
     if (t < tmin || t > tmax){
         return false;
-    Vec3 p = r.at(t);
-    Vec3 rel = p - origin;
-    double u = dot(rel, u_dir);
-    double v = dot(rel, v_dir);
-    if (u < umin || u > umax || v < vmin || v > vmax){
+    }
+    
+    Vec3 p = r.at(t);                                   //
+    Vec3 rel = p - origin;                              //
+    double u = dot(rel, u_dir);                         // --> checks if the intersection pointt lies in the bound or not , if the coordinates lies outside the bound then ray hits outside the rectangle  
+    double v = dot(rel, v_dir);                         //
+    if (u < umin || u > umax || v < vmin || v > vmax){  //
          return false;
     }
-    rec.t = t; rec.p = p; rec.set_face_normal(r, normal); 
-    rec.mat = mat;
-    return true;
+                                        
+    rec.t = t;                         //
+    rec.p = p;                         //
+    rec.set_face_normal(r, normal);    // --> stores hit distance fron the ray origin , intersection point , normal and the material 
+    rec.mat = mat;                     // 
+    return true;                       //
 }
-
+// function to build the box around the sphere by making 4 rectangles using the points with x , y, z coordinates 
 bool RectPlane::bounding_box(double, double, AABB &box) const {
     Vec3 corners[4];
     corners[0] = origin + umin*u_dir + vmin*v_dir;
@@ -73,7 +88,7 @@ bool RectPlane::bounding_box(double, double, AABB &box) const {
         maxp.y = std::max(maxp.y, corners[i].y);
         maxp.z = std::max(maxp.z, corners[i].z);
     }
-    Vec3 t = normal * EPS_THICK;
+    Vec3 t = normal * EPS_THICK; // provides some thickness along the normal to avoid the zero volume box 
     minp = minp - t;
     maxp = maxp + t;
     box = AABB(minp, maxp);
