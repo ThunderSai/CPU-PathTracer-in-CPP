e4322410798b7d46a40fba5ca963d737abbe5ebc
madhurvishwakarma <b24ee1041@iitj.ac.in>
2025-11-14 19:17:34 +0530

    Update main.cpp

--- with_bvh/main.cpp
+++ with_bvh/main.cpp
@@ -11,16 +11,17 @@
 
 using namespace std;
 using namespace std::chrono;
-
+// function to calclate the color of each pixel , here the costants r = ray from camera , world = it is basically the scene containing all objects , depth = working basically as recursion limit 
 Vec3 trace(const Ray &r, const Hittable &world, int depth) {
     if (depth <= 0) return Vec3(0,0,0);
-
+    // below function is to check whether the ray hits any object or not and if hit then take its record 
+    // we can say that this is the heart or core of path tracer 
     HitRecord rec;
-    
     if (world.hit(r, 1e-4, 1e8, rec)) {
-        Vec3 emitted = rec.mat->emitted();
+        Vec3 emitted = rec.mat->emitted(); // to get emitted light from the object 
         Ray scattered;
         Vec3 atten;
+        // to trace the scattered ray if scattered from the object 
         if (rec.mat->scatter(r, rec, atten, scattered)) {
             Vec3 col = trace(scattered, world, depth-1);
             return emitted + atten * col;
@@ -29,25 +30,26 @@ Vec3 trace(const Ray &r, const Hittable &world, int depth) {
         }
     }
 
-    return Vec3(0,0,0);
+    return Vec3(0,0,0); // if ray hits all the object and goes into empty space then return black 
 }
 
 int main(){
+    // basically it is the timing and performance setting for our path tracer code 
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
+// code to start the rendering process 
     auto start_time = high_resolution_clock::now();
     cerr << "Starting render (WITH BVH)...\n";
-
-    const int WIDTH = 1920;
-    const int HEIGHT = 1080;
-    const int SAMPLES = 100;
-    const int MAX_DEPTH = 12;
-
+// the parameters below are basically the image parameters or can be said to be image parameters 
+    const int WIDTH = 1920;   //
+    const int HEIGHT = 1080;  // these are basically the pixel dimensions 
+    const int SAMPLES = 100;   // these are basically the no. of rays per pixel 
+    const int MAX_DEPTH = 12;   // can be sid to be max recusion limit or recursion depth 
+// image header in PPM format 
     cout << "P3\n" << WIDTH << " " << HEIGHT << "\n255\n";
-
+// to setup the scenes or we can say that it is to store all the  objects 
     vector<shared_ptr<Hittable>> objects;
-
+// the below code is to define the materials 
     auto red = make_shared<Lambert>(Vec3(0.65, 0.05, 0.05));
     auto green = make_shared<Lambert>(Vec3(0.12, 0.45, 0.15));
     auto grey = make_shared<Lambert>(Vec3(0.73, 0.73, 0.73));
@@ -56,20 +58,20 @@ int main(){
     auto glass = make_shared<Dielectric>(1.5);
     auto window_left_light = make_shared<DiffuseLight>(Vec3(3.0, 3.5, 4.0));
     auto window_right_light = make_shared<DiffuseLight>(Vec3(3.0, 1.0, 0.2));
-
+// the below code is to define the 3D room geometry 
     double room_len = 2.0;
     double half = room_len / 2.0;
-
-    objects.push_back(make_shared<RectPlane>(Vec3(0,-half,0), Vec3(0,1,0), Vec3(1,0,0), Vec3(0,0,1),
-                                     -half, half, -half, half, grey));
-    objects.push_back(make_shared<RectPlane>(Vec3(0,half,0), Vec3(0,-1,0), Vec3(1,0,0), Vec3(0,0,1),
-                                     -half, half, -half, half, grey));
-    objects.push_back(make_shared<RectPlane>(Vec3(0,0,-half), Vec3(0,0,1), Vec3(1,0,0), Vec3(0,1,0),
-                                     -half, half, -half, half, grey));
-    objects.push_back(make_shared<RectPlane>(Vec3(-half,0,0), Vec3(1,0,0), Vec3(0,0,1), Vec3(0,1,0),
-                                     -half, half, -half, half, red));
-    objects.push_back(make_shared<RectPlane>(Vec3(half,0,0), Vec3(-1,0,0), Vec3(0,0,1), Vec3(0,1,0),
-                                     -half, half, -half, half, green));
+                                                                                                    
+    objects.push_back(make_shared<RectPlane>(Vec3(0,-half,0), Vec3(0,1,0), Vec3(1,0,0), Vec3(0,0,1), //
+                                     -half, half, -half, half, grey));                               //
+    objects.push_back(make_shared<RectPlane>(Vec3(0,half,0), Vec3(0,-1,0), Vec3(1,0,0), Vec3(0,0,1), //
+                                     -half, half, -half, half, grey));                               //
+    objects.push_back(make_shared<RectPlane>(Vec3(0,0,-half), Vec3(0,0,1), Vec3(1,0,0), Vec3(0,1,0), // --> it is basically to add the rectangular walls using rectplane  by defining 
+                                     -half, half, -half, half, grey));                               //        left , right , floor , cieling and side walls 
+    objects.push_back(make_shared<RectPlane>(Vec3(-half,0,0), Vec3(1,0,0), Vec3(0,0,1), Vec3(0,1,0), //
+                                     -half, half, -half, half, red));                                //
+    objects.push_back(make_shared<RectPlane>(Vec3(half,0,0), Vec3(-1,0,0), Vec3(0,0,1), Vec3(0,1,0), //
+                                     -half, half, -half, half, green));                              //
 
     objects.push_back(make_shared<RectPlane>(Vec3(-half+1e-4, 0.3, -0.6), Vec3(1,0,0),
                                      Vec3(0,0,1), Vec3(0,1,0),
