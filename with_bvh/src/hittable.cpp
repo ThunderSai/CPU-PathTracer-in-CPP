b1b512fa953439388297cc01b223fab4793b3bab
Sairaj Mestry <mestry.sairaj1506@gmail.com>
2025-11-15 16:37:00 +0530

    Update hittable.cpp

--- with_bvh/src/hittable.cpp
+++ with_bvh/src/hittable.cpp
@@ -96,80 +96,98 @@ bool RectPlane::bounding_box(double, double, AABB &box) const {
 }
 
 BVHNode::BVHNode(std::vector<std::shared_ptr<Hittable>> &objects, size_t start, size_t end) {
+    // Create a random number generator (used to pick a random axis: x, y, or z)
     std::random_device rd;
     std::default_random_engine rng(rd());
+
+    // Pick a random axis: 0 = x, 1 = y, 2 = z
     int axis = int(3 * rand01());
+    // Choose which comparison function to use depending on the axis
     auto comparator = (axis == 0) ? box_x_compare
                     : (axis == 1) ? box_y_compare
                                   : box_z_compare;
-
+    
+    // Number of objects in this part of the list
     size_t object_span = end - start;
-
+    // CASE 1: Only one object → both children point to the same object
     if (object_span == 1) {
         left = right = objects[start];
-    } else if (object_span == 2) {
+    } else if (object_span == 2) {     // CASE 2: Two objects → simply compare and assign left/right
         if (comparator(objects[start], objects[start+1])) {
             left = objects[start];
             right = objects[start+1];
-        } else {
+        } else { 
             left = objects[start+1];
             right = objects[start];
         }
-    } else {
+    } else {                          // CASE 3: More than two objects → split the list into two halves
+        // Sort objects along the chosen axis (x or y or z)
         std::sort(objects.begin() + start, objects.begin() + end, comparator);
+        // Middle index for dividing the list
         size_t mid = start + object_span / 2;
+
+        // Recursively build left and right BVH nodes
         left = std::make_shared<BVHNode>(objects, start, mid);
         right = std::make_shared<BVHNode>(objects, mid, end);
     }
 
-    AABB box_left, box_right;
+    AABB box_left, box_right;  // Get the bounding boxes of both children
     if (!left->bounding_box(0,0, box_left) || !right->bounding_box(0,0, box_right))
         std::cerr << "No bounding box in BVHNode constructor.\n";
+
+    // The bounding box of this node is the box that contains both child boxes
     box = surrounding_box(box_left, box_right);
 }
 
 bool BVHNode::hit(const Ray &r, double tmin, double tmax, HitRecord &rec) const {
+    // If the ray does NOT hit this node's bounding box, then it cannot hit anything inside it
     if (!box.hit(r, tmin, tmax)) return false;
     HitRecord leftRec, rightRec;
     
+    // Try hitting the left child
+    // then try hitting the right child
+    // NOTE: If left side was hit, we limit tmax to the left hit distance
+    // This helps us choose the closest hit
     bool hit_left = left->hit(r, tmin, tmax, leftRec);
     bool hit_right = right->hit(r, tmin, hit_left ? leftRec.t : tmax, rightRec);
-
+    // If right side is hit, return that (right is closer OR left not hit)
     if (hit_right) {
         rec = rightRec;
         return true;
     }
-
+    // If only left side is hit, return that
     if (hit_left) {
         rec = leftRec;
         return true;
     }
-
-    return false;
+                   
+    return false; // Nothing hit in this BVH node
 }
 
 bool BVHNode::bounding_box(double, double, AABB &out_box) const {
-    out_box = box;
+    out_box = box;  // Simply give back the stored bounding box
     return true;
 }
 
 bool BVHNode::box_compare(const std::shared_ptr<Hittable> a, const std::shared_ptr<Hittable> b, int axis) {
     AABB box_a, box_b;
-    if (!a->bounding_box(0,0,box_a) || !b->bounding_box(0,0,box_b))
+    if (!a->bounding_box(0,0,box_a) || !b->bounding_box(0,0,box_b))    // Each object must have a bounding box
         std::cerr << "No bounding box in box_compare.\n";
-    if (axis == 0) return box_a.min().x < box_b.min().x;
-    else if (axis == 1) return box_a.min().y < box_b.min().y;
-    else return box_a.min().z < box_b.min().z;
+
+    // Compare the minimum corner of each box along the chosen axis
+    if (axis == 0) return box_a.min().x < box_b.min().x;  //compare on x 
+    else if (axis == 1) return box_a.min().y < box_b.min().y; //compare on y
+    else return box_a.min().z < box_b.min().z; //compare on z
 }
 
 bool BVHNode::box_x_compare(const std::shared_ptr<Hittable> a, const std::shared_ptr<Hittable> b) { 
-    return box_compare(a,b,0); 
+    return box_compare(a,b,0); // Compare using x axis
 }
 
 bool BVHNode::box_y_compare(const std::shared_ptr<Hittable> a, const std::shared_ptr<Hittable> b) { 
-    return box_compare(a,b,1); 
+    return box_compare(a,b,1); // Compare using y axis
 }
 
 bool BVHNode::box_z_compare(const std::shared_ptr<Hittable> a, const std::shared_ptr<Hittable> b) { 
-    return box_compare(a,b,2); 
+    return box_compare(a,b,2); // Compare using z axis
 }
